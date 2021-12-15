54476e51ff27cdd833024da0f77721456652cead
Vinícius Resende <92769975+Vinicius-resende-cin@users.noreply.github.com>
2021-12-14 22:39:27 -0300

    resolvendo conflitos com a main

--- source/Main.py
+++ source/Main.py
@@ -3,6 +3,7 @@ from Tela import Tela
 from Personagem import *
 from Cores import coresRGB
 from Coletaveis import *
+from Labirinto import Labirinto
 
 
 class Main:
@@ -94,36 +95,46 @@ class Main:
 
         # Objetos:
         # criando a chave
+        efeito_coleta_de_keys = pygame.mixer.Sound(
+            'assets/sounds/fesliyan studios_collecting _keys_opção 01.wav')
         imagem_chave = pg.image.load("assets/Coletaveis/chave.png")
         pos_x = 500
         pos_y = 230
-        chave = Chave(pos_x, pos_y, self.superficie, imagem_chave)
+        chave = Chave(pos_x, pos_y, self.superficie,
+                      imagem_chave, efeito_coleta_de_keys)
 
         # criando os relogios
+        efeito_coleta_de_relógios = pygame.mixer.Sound(
+            'assets/sounds/orange free sounds_collecting_items_opção 04.wav')
         pos_x = 300
         pos_y = 230
-        tempo = Relogio(pos_x, pos_y, self.superficie)
-        tempo = Relogio(pos_x+50, pos_y, self.superficie)
+        tempo = Relogio(pos_x, pos_y, self.superficie,
+                        efeito_coleta_de_relógios)
+        tempo = Relogio(pos_x+50, pos_y, self.superficie,
+                        efeito_coleta_de_relógios)
 
         # criando as moedas
+        efeito_coleta_de_moedas = pygame.mixer.Sound(
+            'assets/sounds/freesound_collecting_coins_opção 01.wav')
         imagem_moeda = pg.image.load("assets/Coletaveis/moeda.png")
         pos_y = 230
         pos_x = 500
         for _ in range(3):
             pos_x += 70
-            moeda = Moeda(pos_x, pos_y, self.superficie, imagem_moeda)
+            moeda = Moeda(pos_x, pos_y, self.superficie,
+                          imagem_moeda, efeito_coleta_de_moedas)
 
         # cria o labirinto
-        imagem_labirinto = pg.image.load("assets/imagens/teste_colisao.png")
-        labirinto = pg.sprite.Sprite()
-        labirinto.rect = imagem_labirinto.get_rect()
-        labirinto.image = pg.transform.scale(imagem_labirinto, (800, 600))
+        imagem_labirinto = pg.image.load("assets/imagens/fundo.png")
+        paredes_labirinto = pg.image.load("assets/imagens/Labirinto.png")
+        labirinto = Labirinto(
+            imagem_labirinto, paredes_labirinto, self.superficie)
 
         # cria o personagem
         imagem_personagem = pg.image.load(
             "assets/Personagem/imagens/player.png")
-        personagem = Personagem(self.superficie, labirinto,
-                                20, 230, 40, 40, 5, coresRGB["azul"], imagem_personagem)
+        personagem = Personagem(self.superficie, labirinto.parede, labirinto.piso,
+                                20, 250, 35, 35, 5, coresRGB["azul"], imagem_personagem)
 
         while self.jogando:
             for evento in pg.event.get():
