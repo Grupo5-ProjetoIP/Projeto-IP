ba106dff41eb0010ddf5afb060a0ab68c92244e3
Tomkcatel <95656864+Tomkcatek@users.noreply.github.com>
2021-12-14 21:07:55 -0300

    Add files via upload

--- source/Main.py
+++ source/Main.py
@@ -1,8 +1,10 @@
+import pygame as pg
+
+from contador import Contador
 from Tela import Tela
 from Personagem import *
 from Cores import coresRGB
-from Coletaveis import *
-
+from Contador import contador
 
 def main():
     """função principal do programa"""
@@ -11,50 +13,26 @@ def main():
     superficie = tela.criar_tela()
 
     clock = pg.time.Clock()
-
-    # Objetos:
-    # criando a chave
-    imagem_chave = pg.image.load("assets/Coletaveis/chave.png")
-    pos_x = 500
-    pos_y = 230
-    chave = Chave(pos_x, pos_y, superficie, imagem_chave)
-
-    # criando os relogios
-    pos_x = 300
-    pos_y = 230
-    tempo = Relogio(pos_x, pos_y, superficie)
-    tempo = Relogio(pos_x+50, pos_y, superficie)
-
-    # criando as moedas
-    imagem_moeda = pg.image.load("assets/Coletaveis/moeda.png")
-    pos_y = 230
-    pos_x = 500
-    for _ in range(3):
-        pos_x += 70
-        moeda = Moeda(pos_x, pos_y, superficie, imagem_moeda)
-
-    imagem_labirinto = pg.image.load("assets/imagens/teste_colisao.png")
-    labirinto = pg.sprite.Sprite()
-    labirinto.rect = imagem_labirinto.get_rect()
-    labirinto.image = pg.transform.scale(imagem_labirinto, (800, 600))
+    
+    contador=contador(superficie,10000)
 
     # cria o personagem
-    imagem_personagem = pg.image.load("assets/Personagem/imagens/player.png")
-    personagem = Personagem(superficie, labirinto,
-                            20, 230, 40, 40, 5, coresRGB["azul"], imagem_personagem)
+    imagem_personagem = pg.image.load("assets/Personagem/imagens/teste.png")
+    personagem = Personagem(superficie, 400, 300, 40,
+                            40, 10, coresRGB["azul"], imagem_personagem)
 
     rodando = True
     while rodando:
-        for evento in pg.event.get():
+        for event in pg.event.get():
             # sai do jogo
-            if evento.type == pg.QUIT:
+            if event.type == pg.QUIT or contador>=0:
                 rodando = False
+                
+            
+        contador.contar()
+        
 
         personagem.update()
-
-        tempo.update(personagem)
-        chave.update(personagem)
-        moeda.update(personagem)
 
         pg.display.flip()
         clock.tick(30)
