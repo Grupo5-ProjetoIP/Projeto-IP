6e66efef8cce240be8dc48a035aac59bf92b573d
Tomkcatel <95656864+Tomkcatek@users.noreply.github.com>
2021-12-14 21:06:55 -0300

    Add files via upload

---
+++ source/contador.py
@@ -0,0 +1,29 @@
+import pygame as pg
+
+
+class contador():
+    def __init__(self, janela,c):
+    
+
+        self.cor=(0,0,0)
+        self.janela=janela
+        self.tempo=c
+        
+        self.fonte=pg.font.Font(None,49)
+        self.texto=fonte.render("Tempo:" + str(c), True, self.cor)
+        
+        self.start=True
+    
+    def contar(self):
+        self.c-=1
+        
+        
+        self.janela.fill((255,255,255))
+        self.janela.blit(self.texto,[0,0])
+        minutos=self.tempo//60000
+        segundos=self.tempo//1000
+        self.texto=fonte.render("Tempo:" + f'{minutos}:{segundos}', True,self.cor)
+
+        
+
+    
